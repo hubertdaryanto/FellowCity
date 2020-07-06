00c404bb35819b93372298747d96b934cec248ca
Hubert Darryl Daryanto <partishion@gmail.com>
2020-07-06 11:23:02 +0700

    test feature 1

---
+++ Rename2/Feature1/Feature1.swift
@@ -0,0 +1,14 @@
+//
+//  Feature1.swift
+//  Rename2
+//
+//  Created by Hubert Daryanto on 06/07/20.
+//  Copyright © 2020 Hubert Daryanto. All rights reserved.
+//
+
+import Foundation
+
+func feature1()
+{
+    
+}
