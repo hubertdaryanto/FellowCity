be760a82676502b423a5ba0128ac85865a464157
Feri Fajri <feri.okana@gmail.com>
2020-07-20 18:36:48 +0700

    1st commit feature explore

---
+++ Aplikasi Motor/View/ExploreView.swift
@@ -0,0 +1,21 @@
+//
+//  ExploreView.swift
+//  Aplikasi Motor
+//
+//  Created by Feri Fajri on 20/07/20.
+//  Copyright © 2020 Hubert Daryanto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
